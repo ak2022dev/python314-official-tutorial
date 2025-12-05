fc435fb9d9b4a5a55802d7b671ff1844f21b1131
Amir Khan <ak2022dev@gmail.com>
2025-12-05 12:25:06 +0000

    Add second for, including collection, copy, del and iteration.

---
+++ second_for.py
@@ -0,0 +1,14 @@
+# Create a sample collection
+users = { 'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active' }
+
+# Strategy: iterate over a copy
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+
+# Strategy: Create a new collection
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status#
+
