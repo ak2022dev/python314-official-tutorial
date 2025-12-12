b1d0b81933be611747776a060ebdedb66a7ba592
Amir Khan <ak2022dev@gmail.com>
2025-12-12 10:58:02 +0000

    Add further examples into range_first.py

--- range_first.py
+++ range_first.py
@@ -1,4 +1,4 @@
-print("range(5) results in")
+print("range(5) results in:")
 for i in range(5):
     print(i, end=' ')
 print()
@@ -12,7 +12,11 @@ print(list(range(0,10,3)))
 print("list(range(-10,-100,-30)) results in:")
 print(list(range(-10, -100, -30)))
 
+print("With list a = ['Mary', 'had', 'a', 'little', 'lamb'] printing i, a[i] with range(len(a)) results in:")
 a = ['Mary', 'had', 'a', 'little', 'lamb']
 for i in range(len(a)):
     print(i, a[i])
+
+print("sum(range(4)) results in:")
+print(sum(range(4)))
 
