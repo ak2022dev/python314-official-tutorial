5a069fafe6e15a895ed0cd53f54df669fd3edd9b
Amir Khan <ak2022dev@gmail.com>
2025-12-12 12:08:30 +0000

    Add use of | to match_first.py.

--- match_first.py
+++ match_first.py
@@ -3,8 +3,8 @@ def http_error(status):
     match status:
         case 400:
             return "bad request"
-        case 404:
-            return "Not found"
+        case 401 | 402 | 403| 404:
+            return "Not allowed"
         case 418:
             return "I'm a teapot"
         case _:
@@ -13,6 +13,9 @@ def http_error(status):
 # the last case, variable _, acts as a wildcard that never fails to match
 
 print("http_error(400):", http_error(400))
+print("http_error(401):", http_error(401))
+print("http_error(402):", http_error(402))
+print("http_error(403):", http_error(403))
 print("http_error(404):", http_error(404))
 print("http_error(418):", http_error(418))
 print("http_error(419):", http_error(419))
