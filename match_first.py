bbfa4e2e331bd26413211653f761371d08100123
Amir Khan <ak2022dev@gmail.com>
2025-12-12 12:03:47 +0000

    Add first match example, starting with literals.

---
+++ match_first.py
@@ -0,0 +1,18 @@
+# Simplest form of match compares against one or more literals
+def http_error(status):
+    match status:
+        case 400:
+            return "bad request"
+        case 404:
+            return "Not found"
+        case 418:
+            return "I'm a teapot"
+        case _:
+            return "Something's wrong with the internet"
+
+# the last case, variable _, acts as a wildcard that never fails to match
+
+print("http_error(400):", http_error(400))
+print("http_error(404):", http_error(404))
+print("http_error(418):", http_error(418))
+print("http_error(419):", http_error(419))
