7bfdf305cc6d964c3ce23d3a71796ad8d3be8e5b
Amir Khan <ak2022dev@gmail.com>
2025-12-12 12:30:07 +0000

    WIP Add second match example that uses class and variable binding. To test later.

---
+++ match_second.py
@@ -0,0 +1,20 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def where_is(point):
+    
+        match point:
+            case (0, 0):
+                print("Origin")
+            case (0, y):
+                print(f"Y={y}")
+            case (x, 0):
+                print(f"X={x}")
+            case (x, y):
+                print(f"X={x}, Y={y}")
+            case _:
+                raise ValueError("Not a point")
+
+
