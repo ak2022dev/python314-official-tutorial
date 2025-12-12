dd7b0d06d1841b4358803564066dbaac1da3d81d
Amir Khan <ak2022dev@gmail.com>
2025-12-12 11:04:54 +0000

    Add first break and continue examples.

---
+++ break_continue_first.py
@@ -0,0 +1,7 @@
+print("Use nested for with range and % and // to print some factors")
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(f"{n} equals {x} * {n//x}")
+            break
+
